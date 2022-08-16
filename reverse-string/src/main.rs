b52334b8893ebfd3c3e9fb760704225bdb7ef947
egetelli <egetelli@hotmail.com>
2022-08-16 10:53:53 +0300

    reverse

--- reverse-string/src/main.rs
+++ reverse-string/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use unicode_segmentation::UnicodeSegmentation;
+
+pub fn reverse(input: &str) -> String {
+    // unimplemented!("Write a function to reverse {}", input);
+    // input.chars().rev().collect::<String>()
+    // input.chars().rev().collect()
+    // UnicodeSegmentation::graphemes(input, true).rev().collect()
+    input.graphemes(true).rev().collect::<String>()
 }
