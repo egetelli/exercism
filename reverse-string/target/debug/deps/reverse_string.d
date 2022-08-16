562d559add4696abe78f662e3086a97c0e6492c0
egetelli <egetelli@hotmail.com>
2022-08-16 10:47:47 +0300

    aa

---
+++ reverse-string/target/debug/deps/reverse_string.d
@@ -0,0 +1,5 @@
+C:\Users\egete\Desktop\projects\Rust\exercism\reverse-string\target\debug\deps\reverse_string.exe: src\main.rs
+
+C:\Users\egete\Desktop\projects\Rust\exercism\reverse-string\target\debug\deps\reverse_string.d: src\main.rs
+
+src\main.rs:
---
+++ reverse-string/target/debug/reverse-string.d
@@ -0,0 +1,1 @@
+C:\Users\egete\Desktop\projects\Rust\exercism\reverse-string\target\debug\reverse-string.exe: C:\Users\egete\Desktop\projects\Rust\exercism\reverse-string\src\main.rs
